5a56dc4797447da7a65f48accb307ffd9e0f1813
BL4CK DoG <xoxohoon01@gmail.com>
2024-11-06 19:37:12 +0900

    Feat: 총기 제작 추가

---
+++ Assets/GunScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform flashPoint;
+
+    private float fireRate = 3.5f;
+    private float fireDelay;
+
+    public void Fire()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (fireDelay <= 0)
+            {
+                GameObject newBullet = Instantiate(bullet);
+                newBullet.transform.position = flashPoint.position;
+                fireDelay = 1 / fireRate;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (fireDelay > 0)
+            fireDelay -= Time.deltaTime;
+    }
+}
